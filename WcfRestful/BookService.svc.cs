59c68184e1d402273c86fc5b5b848862919c7f5d
wuminggang <wuminggang@huachunnet.com>
2017-03-19 20:00:02 +0800

    添加接口服务实现

---
+++ WcfRestful/BookService.svc.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using WcfRestful.Models;
+
+namespace WcfRestful
+{
+    // 注意: 使用“重构”菜单上的“重命名”命令，可以同时更改代码、svc 和配置文件中的类名“BookService”。
+    // 注意: 为了启动 WCF 测试客户端以测试此服务，请在解决方案资源管理器中选择 BookService.svc 或 BookService.svc.cs，然后开始调试。
+    public class BookService : IBookService
+    {
+        private List<Book> _dataSource = new List<Book>()
+        {
+            new Book() {Id = 1, Name = "西游记", Price = 23},
+            new Book() {Id = 2, Name = "红楼梦", Price = 22},
+            new Book() {Id = 3, Name = "封神榜", Price = 23},
+            new Book() {Id = 4, Name = "金瓶梅", Price = 33},
+            new Book() {Id = 5, Name = "水浒传", Price = 25}
+        }; 
+
+
+        public List<Book> GetList()
+        {
+            return _dataSource;
+        }
+
+        public Book GetById(string id, string name)
+        {
+            int idValue = int.TryParse(id, out idValue) ? idValue : 1;
+            return _dataSource.FirstOrDefault(p => p.Id == idValue);
+        }
+
+        public List<Book> Add(Book model)
+        {
+            if (model == null)
+            {
+                model = new Book();
+            }
+            model.Id = _dataSource.Max(p => p.Id) + 1;
+            _dataSource.Add(model);
+
+            return _dataSource;
+        }
+
+        public List<Book> Update(Book model)
+        {
+            if (model == null)
+                return _dataSource;
+            var book = _dataSource.FirstOrDefault(p => p.Id == model.Id);
+            if (book == null)
+                return _dataSource;
+            book.Name = model.Name;
+            book.Price = model.Price;
+
+            return _dataSource;
+        }
+
+        public List<Book> Delete(string id)
+        {
+            int idValue = int.TryParse(id, out idValue) ? idValue : 1;
+            var model = _dataSource.FirstOrDefault(p => p.Id == idValue);
+            if (model == null)
+                return _dataSource;
+            _dataSource.Remove(model);
+            return _dataSource;
+        } 
+    }
+}
---
+++ WcfRestful/IBookService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Web.Script.Services;
+using WcfRestful.Models;
+
+namespace WcfRestful
+{
+    // 注意: 使用“重构”菜单上的“重命名”命令，可以同时更改代码和配置文件中的接口名“IBookService”。
+    [ServiceContract]
+    public interface IBookService
+    {
+        [WebGet(UriTemplate = "book/list", ResponseFormat = WebMessageFormat.Json)]
+        [OperationContract]
+        List<Book> GetList();
+
+        ///// <summary>
+        ///// 如果调用段有多个查询条件，可以先将查询条件组织成 json 格式， 然后在通过参数 query 传递。后台对参数进行反序列化
+        ///// </summary>
+        ///// <param name="query"></param>
+        ///// <returns></returns>
+        //[WebGet(UriTemplate = "book/list/?query={query}", ResponseFormat = WebMessageFormat.Json)]
+        //[OperationContract]
+        //List<Book> GetList(string query);
+
+
+        [WebGet(UriTemplate = "book/list/{id}/{name}", ResponseFormat = WebMessageFormat.Json)]
+        [OperationContract]
+        Book GetById(string id, string name);
+        
+        [WebInvoke(
+            UriTemplate = "book/add", 
+            Method = "POST", 
+            RequestFormat = WebMessageFormat.Json, 
+            ResponseFormat = WebMessageFormat.Json)]
+        [OperationContract]
+        List<Book> Add(Book model);
+
+
+        [WebInvoke(
+            UriTemplate = "book/update", 
+            Method = "PUT", 
+            RequestFormat = WebMessageFormat.Json, 
+            ResponseFormat = WebMessageFormat.Json)]
+        [OperationContract]
+        List<Book> Update(Book book);
+
+        [WebInvoke(
+            UriTemplate = "book/delete/{id}", 
+            Method = "DELETE", 
+            ResponseFormat = WebMessageFormat.Json)]
+        [OperationContract]
+        List<Book> Delete(string id);
+    }
+}
